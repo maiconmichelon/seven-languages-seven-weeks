33982b8f65c0608ec01c1afd52ae546fd558571d
maiconmichelon <maicon.fermo@gmail.com>
2014-07-17 00:00:03 -0300

    clojure

---
+++ clojure/resumo.clj
@@ -0,0 +1,11 @@
+(println "Give me some Clojure!")
+(println (+ 2 2 2)) ;; 6
+(println (mod 5 4)) ;; 1
+(println (/ (/ 8 2) (/ 4 2))) ;; 2
+(println (/ (/ 6 2) (/ 4 2))) ;; 3/2
+(println (< 1 2 3 4)) ;; true
+(println (< 1 2 5 3)) ;; false
+(println (str "um, " "dois, " " e tres")) ;; um, dois e tres
+(println (str \f \o \r \c \e)) ;; force
+(println (= "a" \a)) ;; false
+(println (= ""))
